6d6c42aa8d11cd2e9a7943a98a9eb2740b2d1ae8
Eduardo Quirino <101748384+edquirino08@users.noreply.github.com>
2022-11-11 10:36:58 -0300

    Initial commit

---
+++ src/main/java/com/analystic/aws/CallAnalysticAmazonApplication.java
@@ -0,0 +1,13 @@
+package com.analystic.aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallAnalysticAmazonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CallAnalysticAmazonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/analystic/aws/CallAnalysticAmazonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.analystic.aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CallAnalysticAmazonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
